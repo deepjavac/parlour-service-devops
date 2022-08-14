c0c48288c377644e4937a218d43c6df6260174ae
Deepak Patel <deepak.patel@gen-xt.com>
2022-08-14 17:24:56 +0530

    first commit

---
+++ src/main/java/com/shadimaterial/parlourservice/ParlourServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shadimaterial.parlourservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParlourServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParlourServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shadimaterial/parlourservice/ParlourServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadimaterial.parlourservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParlourServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
